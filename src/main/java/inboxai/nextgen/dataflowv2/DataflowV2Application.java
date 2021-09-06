a1c90dd19cd0894091fbc50bc33f1a9b27dbdfaf
Guy <guy.gberg@gmail.com>
2021-09-06 16:03:48 +0300

    first commit

---
+++ src/main/java/inboxai/nextgen/dataflowv2/DataflowV2Application.java
@@ -0,0 +1,13 @@
+package inboxai.nextgen.dataflowv2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataflowV2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataflowV2Application.class, args);
+	}
+
+}
---
+++ src/test/java/inboxai/nextgen/dataflowv2/DataflowV2ApplicationTests.java
@@ -0,0 +1,13 @@
+package inboxai.nextgen.dataflowv2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataflowV2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
